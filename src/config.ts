81ab2303cdc1e3e320b8c46615bbcad1947c718d
Vadim L. Avramishin <avramishin@gmail.com>
2024-03-13 15:27:38 +0300

    add env file

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,24 @@
+require('dotenv').config({ path: __dirname + '/../.env' });
+
 import { uuidv4 } from './common/uuid-v4';
 
-const api_key = 'cfda9da8-1c8a-443a-8a33-da676540fa84';
-const api_secret = 'WWXKIAYXI6EDJTG0WN0LDRRYQ4TZ4N5MS1KHZTNCHWP';
+const api_key = process.env['EXCHANGE_API_KEY'];
+const api_secret = process.env['EXCHANGE_API_SECRET'];
 
 export const config = {
   app_id: uuidv4().slice(0, 6),
   db: {
     migrate: true,
     type: 'mysql',
-    host:
-      process.env['DATABASE_HOST'] ||
-      'vakotrade.cxgliye7pvfd.us-east-1.rds.amazonaws.com',
-    user: process.env['DATABASE_USERNAME'] || 'bitso',
-    password: process.env['DATABASE_PASSWORD'] || 'La6no5N2',
-    database: process.env['DATABASE_NAME'] || 'bitso',
+    host: process.env['DATABASE_HOST'] || '',
+    user: process.env['DATABASE_USERNAME'] || '',
+    password: process.env['DATABASE_PASSWORD'] || '',
+    database: process.env['DATABASE_NAME'] || '',
     port: process.env['DATABASE_PORT'] || '3306',
   },
 
   bitso: {
-    webhook: 'https://webhook.site/182b2027-fd73-4318-9442-933bc75cb2ea',
+    webhook: process.env['BITSO_WEBHOOK'],
   },
 
   admins: [
